6ebcb859295ccae8bc81e40b5c923823c187f192
AnnaSems <sem.mm@mail.ru>
2023-04-19 00:35:34 +1000

    добавлены шаблоны html, css, img

---
+++ Итоговый_проект/posts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+
+# Главная страница
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+# Дизайн
+def design_page(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+# Веб-разработка
+def web_dev_page(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+# Мобильная разработка
+def mob_dev_page(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+# Маркетинг
+def market_page(request):
+    template = 'posts/index.html'
+    return render(request, template)
