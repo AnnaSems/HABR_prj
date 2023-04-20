cca1efcf3f4ce82160d5070a4be45378a3240f73
AnnaSems <sem.mm@mail.ru>
2023-04-20 16:53:20 +1000

    изменено оформление, добавлена пагинация

--- Итоговый_проект/posts/views.py
+++ Итоговый_проект/posts/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from . models import Post
 
 
-# Главная страница
+def authorized_only(func):
+    def check_user(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return func(request, *args, **kwargs)
+        return redirect('/auth/login/')
+    return check_user
+
+
+# Главная страницаs
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
@@ -14,8 +27,11 @@ def index(request):
 # Дизайн
 def design_page(request):
     posts = Post.objects.filter(category='Design').order_by('-pub_date')[:10]
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     template = 'posts/design.html'
     return render(request, template, context)
@@ -25,8 +41,11 @@ def design_page(request):
 def web_dev_page(request):
     posts = Post.objects.filter(
         category='Web').order_by('-pub_date')[:10]
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     template = 'posts/web_dev.html'
     return render(request, template, context)
@@ -35,8 +54,11 @@ def web_dev_page(request):
 # Мобильная разработка
 def mob_dev_page(request):
     posts = Post.objects.filter(category='Mobile').order_by('-pub_date')[:10]
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     template = 'posts/mob_dev.html'
     return render(request, template, context)
@@ -46,8 +68,11 @@ def mob_dev_page(request):
 def market_page(request):
     posts = Post.objects.filter(
         category='Marketing').order_by('-pub_date')[:10]
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     template = 'posts/market.html'
     return render(request, template, context)
